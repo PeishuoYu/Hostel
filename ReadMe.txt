46adf4069b46e544dc88b3bca4c110f95c649f62
PeishuoYu <36442042+PeishuoYu@users.noreply.github.com>
2018-03-26 04:14:46 -0500

    Update

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,12 +2,11 @@ Why I developed this program:
 	Recently I am doing a marketing research about hostel in Denver, and I need the
 	data to find out what customer care about.
 
-	After getting the data, I wanted to categorize the words in review, and make
-	these words dummy variables. Then I hoped to apply the machine learning program
-	to make a model. Finally, by looking at the model, I will be able to tell which
-	group of customers care what. However, my program told me that the preferences
-	of different groups were very similar, which means my plan did not work. Still
-	a good try though :-)
+	In this project, I conducted a word frequency analysis in hope to find out whether
+	different groups of people care about different things about a hostel. If the 
+	answer is yes, I can further tailor different messages delivered to different 
+	groups of people via Facebook ads. However, after gathering and processing data,
+	I found that all groups caring about very similar attributes of a hostel.
 
 In this folder, there are:
 	Hostel.py	A webcrawler that gets the review data from HostelWorld website
@@ -15,11 +14,8 @@ In this folder, there are:
 	los-angeles.csv	The review data for all the hostels in Los Angeles (for demonstration)
 	The Anderson 	The review data for Anderson Estates Hostel (for demonstration)
 	Estates.csv
-	GeneralModel	The machine learning program that is used to build model to
-	Builder.py	find out what different traveling groups care about
 	TextToDummy.py	A program that categorizes the words in reviews, turns
 			these words into dummy variables, and stores them in a csv file
-	model.txt	A model generated my the machine learning program
 
 Major Update 02/15/2018:
 	Added function to automatically generate csv file to store the data collected
--- TextToDummy.py
+++ TextToDummy.py
@@ -16,65 +16,72 @@ useless_words = ['i', 'we', 'our', 'loved', 'felt', 'is', 'in', 'the', 'were', '
                  'has', 'by', 'didn', 'pretty', 'day', 'lots', 'amp', 'outside', 'fun', 'close', 'back', 'fish', 'over'
                  'other']
 
-field = ['Group']
-data = []
-with open('denver.csv', encoding='utf-8', newline='\n') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for review in reader:
-        review_detail = {}
-        review_detail['Group'] = review['gender'] + review['age']
-        text_review = review['text review']
-        words = set(wordfreq.tokenize(text_review, 'en'))
-        for i in words:
-            if i in ['housekeeping', 'professional', 'friendliness', 'staff', 'friendly', 'helpful', 'manager',
-                     'management', 'service']:
-                i = 'staff'
-            elif i in ['cleanest', 'smelled','disgusting', 'clean', 'cleaning', 'cleaner', 'cleanliness', 'smelly',
-                       'cleaned', 'dirty']:
-                i = 'clean'
-            elif i in ['room', 'rooms']:
-                i = 'room'
-            elif i in ['mattresses', 'slept', 'unfortunately', 'uncomfortable', 'bed', 'beds', 'comfortable',
-                       'mattress', 'comfy', 'sleep', 'sleeping', 'zoo']:
-                i = 'bed'
-            elif i in ['towel', 'restrooms', 'restroom', 'door', 'doors', 'towels', 'showering', 'bathroom',
-                       'shower', 'washroom', 'bath', 'toilet']:
-                i = 'bathroom'
-            elif i in ['nightclubs', 'cinema', 'adjacent', 'karaoke', 'locations', 'theatre', 'distance', 'far',
-                       'located', 'shops',  'zoo', 'stadium', 'bar', 'bars', 'downtown', 'restaurant',
-                       'restaurants', 'stores', 'location', 'areas']:
-                i = 'location'
-            elif i in ['music', 'quiet', 'loud', 'atmosphere', 'noise', 'noisy', 'earplugs']:
-                i = 'atmosphere'
-            elif i in ['window', 'laundry', 'kitchens', 'sofas', 'facility', 'light', 'pool', 'ac', 'air', 'locker',
-                       'internet', 'backyard', 'fans', 'sofa', 'curtains', 'walls', 'tv', 'wifi', 'microwave',
-                       'fan', 'lockers', 'kitchen', 'cooking', 'facilities', 'parking', 'desk']:
-                i = 'facilities'
-            elif i in ['walk', 'walking', 'taxi', 'transportation', 'road', 'train', 'buses', 'uber', 'bus',
-                       'station', 'convenient', 'traffic', 'highway', 'freeway', 'metro', 'shuttle', 'transport']:
-                i = 'transportation'
-            elif i in ['paid', 'affordable', 'price', 'free', 'value', 'worth', 'budget', 'cheap', 'cheaper',
-                       'payment', 'dollars', 'purchase''money', 'save', 'expensive', 'pay', 'deal']:
-                i = 'value'
-            elif i in ['dangerous', 'stolen', 'guns', 'unsafe', 'safe', 'secure', 'safety', 'security', 'creepy' ]:
-                i = 'security'
-            elif i in ['private', 'privacy']:
-                i = 'private'
-            elif i in ['activities', 'tours', 'events', 'social', 'attractions', 'parties', 'drinking',
-                       'attraction']:
-                i = 'activities'
-            if i not in useless_words and not i.isdigit():
-                if i not in field:
-                    field.append(i)
-                review_detail[i] = 1
-        data.append(review_detail)
-    print(len(data))
-    csvfile.close()
+def main(fileName, age, gender):
+    field = []
+    data = []
+    with open(fileName, encoding='utf-8', newline='\n') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for review in reader:
+            review_detail = {}
+            if review['age'] == age and review['gender'] == gender:
+                text_review = review['text review']
+                words = set(wordfreq.tokenize(text_review, 'en'))
+                for i in words:
+                    if i in ['housekeeping', 'professional', 'friendliness', 'staff', 'friendly', 'helpful', 'manager',
+                             'management', 'service']:
+                        i = 'staff'
+                    elif i in ['cleanest', 'smelled','disgusting', 'clean', 'cleaning', 'cleaner', 'cleanliness', 'smelly',
+                               'cleaned', 'dirty']:
+                        i = 'clean'
+                    elif i in ['room', 'rooms']:
+                        i = 'room'
+                    elif i in ['mattresses', 'slept', 'unfortunately', 'uncomfortable', 'bed', 'beds', 'comfortable',
+                               'mattress', 'comfy', 'sleep', 'sleeping']:
+                        i = 'bed'
+                    elif i in ['towel', 'restrooms', 'restroom', 'door', 'doors', 'towels', 'showering', 'bathroom',
+                               'shower', 'washroom', 'bath', 'toilet']:
+                        i = 'bathroom'
+                    elif i in ['nightclubs', 'cinema', 'adjacent', 'karaoke', 'locations', 'theatre', 'distance', 'far',
+                               'located', 'shops', 'zoo', 'stadium', 'bar', 'bars', 'downtown', 'restaurant',
+                               'restaurants', 'stores', 'location', 'areas']:
+                        i = 'location'
+                    elif i in ['music', 'quiet', 'loud', 'atmosphere', 'noise', 'noisy', 'earplugs']:
+                        i = 'atmosphere'
+                    elif i in ['window', 'laundry', 'kitchens', 'sofas', 'facility', 'light', 'pool', 'ac', 'air', 'locker',
+                               'internet', 'backyard', 'fans', 'sofa', 'curtains', 'walls', 'tv', 'wifi', 'microwave',
+                               'fan', 'lockers', 'kitchen', 'cooking', 'facilities', 'parking', 'desk']:
+                        i = 'facilities'
+                    elif i in ['walk', 'walking', 'taxi', 'transportation', 'road', 'train', 'buses', 'uber', 'bus',
+                               'station', 'convenient', 'traffic', 'highway', 'freeway', 'metro', 'shuttle', 'transport']:
+                        i = 'transportation'
+                    elif i in ['paid', 'affordable', 'price', 'free', 'value', 'worth', 'budget', 'cheap', 'cheaper',
+                               'payment', 'dollars', 'purchase''money', 'save', 'expensive', 'pay', 'deal']:
+                        i = 'value'
+                    elif i in ['dangerous', 'stolen', 'guns', 'unsafe', 'safe', 'secure', 'safety', 'security', 'creepy' ]:
+                        i = 'security'
+                    elif i in ['private', 'privacy']:
+                        i = 'private'
+                    elif i in ['activities', 'tours', 'events', 'social', 'attractions', 'parties', 'drinking',
+                               'attraction']:
+                        i = 'activities'
+                    if i not in useless_words and not i.isdigit():
+                        if i not in field:
+                            field.append(i)
+                        review_detail[i] = 1
+                data.append(review_detail)
+        csvfile.close()
 
-with open('denver_dummy.csv', 'w', encoding='utf-8', newline='\n') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=field)
-    writer.writeheader()
-    for i in data:
-        writer.writerow(i)
-    csvfile.close()
+    with open(fileName.split('.')[0] + gender + age + '_dummy.csv', 'w', encoding='utf-8', newline='\n') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field)
+        writer.writeheader()
+        for i in data:
+            writer.writerow(i)
+        csvfile.close()
+    print('finish')
+# possible options:
+#   age:        '18-24', '25-30', '31-40', '40+'
+#   gender:     'Female', 'Male', 'Couple', 'MixedGroup', 'AllFemaleGroup', 'AllMaleGroup'
+
+# you have to download the data first using hostel.py
+main(fileName = 'denver.csv', age='18-24', gender='Female')
 
